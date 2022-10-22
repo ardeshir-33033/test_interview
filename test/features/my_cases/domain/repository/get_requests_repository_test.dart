d3f8edd8e5d047bb04ae729834304fde3f80c62d
Ardeshir Ojan <ardeshirojan2@gmail.com>
2022-10-22 04:43:18 +0330

    added some tests

---
+++ test/features/my_cases/domain/repository/get_requests_repository_test.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:test_interview/features/my_cases/domain/repository/get_requests_repository.dart';
+
+@GenerateMocks([GetRequestsRepository])
+void main() {}
--- test/features/my_cases/domain/usecase/get_requests_usecase.dart
+++ test/features/my_cases/domain/usecase/get_requests_usecase.dart
@@ -1,35 +1,35 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mockito/mockito.dart';
+import 'package:test_interview/core/models/response_model.dart';
+import 'package:test_interview/features/my_cases/data/models/request_header_model.dart';
 import 'package:test_interview/features/my_cases/domain/usecase/get_requests_usecase.dart';
 
-
-
-
-
+import '../repository/get_requests_repository_test.mocks.dart';
 
 void main() {
   late GetRequestsUseCase useCase;
   late MockGetRequestsRepository mockGetRequestsRepository;
   setUp(() {
-    mockUserRepository = MockGetRequestsRepository();
+    mockGetRequestsRepository = MockGetRequestsRepository();
     useCase = GetRequestsUseCase(mockGetRequestsRepository);
   });
 
-  final tUser = UserModel.userModelForTest;
-  const int tId = 1;
+  final tReq = RequestHeader.requestHeaderForTest;
+  ResponseModel tRes = ResponseModel();
 
   test(
-    'should pass the user to the user repository and get created user from there',
-        () async {
+    'should pass the request to the request repository ',
+    () async {
       //arrange
-      when(mockUserRepository.createUser(any)).thenAnswer((realInvocation) async => const Right(tId));
+      when(mockGetRequestsRepository.getAllRequests(any, any))
+          .thenAnswer((realInvocation) async => tRes);
       //act
-      final result = await useCase(Params(userEntity: tUser));
+      final result = await useCase(Params(page: 1, tab: "issuance"));
       //assert
-      verify(mockUserRepository.createUser(tUser));
-      expect(result, const Right(tId));
-      verifyNoMoreInteractions(mockUserRepository);
+      verify(mockGetRequestsRepository.getAllRequests("issuance", 1));
+      expect(result, tRes);
+      verifyNoMoreInteractions(mockGetRequestsRepository);
     },
   );
 }
